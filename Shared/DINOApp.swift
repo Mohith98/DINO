cd8e47ef5cf66784f3589a94249dd7c2c78e9f7a
mmohith.98r@gmail.com <dteju0212@gmail.com>
2021-12-02 16:55:16 -0500

    Initial Commit

---
+++ Shared/DINOApp.swift
@@ -0,0 +1,17 @@
+//
+//  DINOApp.swift
+//  Shared
+//
+//  Created by Mohith Muthu on 2021-11-17.
+//
+
+import SwiftUI
+
+@main
+struct DINOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
